5aaa94ab86b80b8be6d6f1bf1cdde91c88c378d0
quicksilver524 <quicksilver524@gmail.com>
2022-06-23 19:47:28 -0400

    updated classes and template

--- src/template.js
+++ src/template.js
@@ -1,46 +1,44 @@
 const { default: generate } = require("@babel/generator");
 const { Console } = require("console");
 
-const user = empData => {
-    if (empData.officeNumber) {
-        return `Office Number: ${empData.officeNumber}`
-    }
-    if (empData.school) {
-        return `School: ${empData.school}`
-    }
-    if (empData.github) {
-        return `<p>Github: <a href="https://github.com/${empData.github}">${empData.github}</a></p>`
-    }
+const user = (empData) => {
+  if (empData.officeNumber) {
+    return `Office Number: ${empData.officeNumber}`;
+  }
+  if (empData.school) {
+    return `School: ${empData.school}`;
+  }
+  if (empData.github) {
+    return `<p class = "click">Github: <a href="https://github.com/${empData.github}">${empData.github}</a></p>`;
+  }
 };
 
-const generateCards = templateData => {
-    return `
+const generateCards = (templateData) => {
+  return `
             <body>
                 <div class="header-wrapper">
                     <h1>Team Profiles</h1>
                 </div>
-                <div class = "grid">
-                ${templateData.map((emp) => {
-        return `
+                <div class = "Cards">
+                ${templateData
+                  .map((emp) => {
+                    return `
                     <div>
                         <h2>${emp.getName()}</h2>
                         <h3>${emp.getRole()}</h3>
                         <p>${emp.getId()}</p>
-                        <p>Email: <a href="mailto:https://${emp.getEmail()}">${emp.getEmail()}</a></p>
+                        <p class = "click">Email: <a href="mailto:https://${emp.getEmail()}">${emp.getEmail()}</a></p>
                         <p>${user(emp)}</p>
                     </div>
-    `
-    }).join('')}
+    `;
+                  })
+                  .join("")}
                 </div>
-            </body>`
-}
+            </body>`;
+};
 
-
-
-
-module.exports = templateData => {
-
-    return `
+module.exports = (templateData) => {
+  return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -52,5 +50,5 @@ module.exports = templateData => {
         </head>   
             ${generateCards(templateData)}                   
         </html>
-        `
+        `;
 };
